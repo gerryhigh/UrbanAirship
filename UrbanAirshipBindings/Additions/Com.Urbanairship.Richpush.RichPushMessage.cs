225f31ec265c1bc7e90615ae7844c2d763403ee6
Gerry High <ghigh@hightg.com>
2013-07-16 07:44:05 -0500

    initial

---
+++ UrbanAirshipBindings/Additions/Com.Urbanairship.Richpush.RichPushMessage.cs
@@ -0,0 +1,10 @@
+namespace Com.Urbanairship.Richpush
+{
+	public partial class RichPushMessage : global::Java.Lang.Object
+	{
+		public virtual int CompareTo(global::Java.Lang.Object p0)
+		{
+			return CompareTo ((Com.Urbanairship.Richpush.RichPushMessage) p0);
+		}
+	}
+}
